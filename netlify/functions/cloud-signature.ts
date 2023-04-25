a357b966f1bd8b303911609a3df2c7bffdde36a1
Oleg Kolisnyk <k4loki@gmail.com>
2023-04-25 19:09:41 +0300

    add cloud signature

--- netlify/functions/cloud-signature.ts
+++ netlify/functions/cloud-signature.ts
@@ -10,44 +10,44 @@ cloudinary.config({
   secure: true,
 });
 
-const handler: Handler = async (event: HandlerEvent, _) => {
+const handler: Handler = async (event, context) => {
   const { headers } = event;
 
+  let admin: GetAdminByIdQuery;
   try {
-    const admin: GetAdminByIdQuery = await getAdminFromHeaders(headers);
-
-    if (!admin.admin_by_pk.id) {
-      return {
-        statusCode: 403,
-        body: JSON.stringify({ message: "Forbidden" }),
-      };
-    }
-
-    const timestamp = Math.round(new Date().getTime() / 1000);
-    const publicId = `menu-${timestamp}`;
-
-    const signature = cloudinary.utils.api_sign_request(
-      {
-        timestamp,
-        public_id: publicId,
-        folder: "menu",
-      },
-      process.env.API_SECRET
-    );
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        signature,
-        timestamp,
-        publicId,
-        apiKey: process.env.API_KEY,
-        cloudName: process.env.CLOUD_NAME,
-      }),
-    };
+    admin = await getAdminFromHeaders(headers);
   } catch (error) {
     return JSON.parse(error.message);
   }
+
+  if (!admin.admin_by_pk?.id) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ message: "Forbidden" }),
+    };
+  }
+
+  const timestamp = Math.round(new Date().getTime() / 1000);
+  const publicId = `menu-${timestamp}`;
+
+  const signature = await cloudinary.utils.api_sign_request(
+    {
+      timestamp: String(timestamp),
+      folder: "menu",
+    },
+    process.env.API_SECRET
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      apiKey: process.env.API_KEY,
+      cloudName: process.env.CLOUD_NAME,
+      signature,
+      timestamp,
+      publicId,
+    }),
+  };
 };
 
 export { handler };
